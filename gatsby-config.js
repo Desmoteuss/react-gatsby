4e0365d6fee04778298bd9e49a803e7fcbc34edc
desmoteuss <desmoteuss@gmail.com>
2020-12-24 16:07:57 +0100

    cleaning code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Travel Webside`,
+    description: `Travel webside showing the places worth to visit`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
