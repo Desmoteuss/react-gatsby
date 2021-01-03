f282ee527ad507df033da27d33d43027fd33f633
desmoteuss <desmoteuss@gmail.com>
2021-01-03 01:20:18 +0100

    adding menu icon

--- src/components/header.js
+++ src/components/header.js
@@ -40,6 +40,11 @@ color: 'blue';
 @media screen and (max-width: 768px) {
   display:block;
   position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate(-100%, 75%);
+  font-size: 1.8rem;
+  cursor: pointer;
 
 }
 `
