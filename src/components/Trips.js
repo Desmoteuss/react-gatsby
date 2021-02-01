2266d822e5da347174e68f35227e973b35985e65
desmoteuss <desmoteuss@gmail.com>
2021-02-01 13:55:55 +0100

    test

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import styled from "styled-components"
 import {useStaticQuery, graphql} from 'gatsby'
+import Img from "gatsby-image"
+
 
 const Trips = () => {
 const data = useStaticQuery(graphql`
